d1361511e657b6b12ab8d5d1fa39b621af8894a4
dnagis <vincent.achard@gmail.com>
2020-03-24 12:53:26 +0100

    lora en spidev direct sur le rpi

--- spi/rpi_lora_spidev.py
+++ spi/rpi_lora_spidev.py
@@ -1,21 +1,72 @@
 #!/bin/python
 import spidev
+from time import sleep
 
 #lora en spidev direct pour homogeneite avec pyftdi
 
+
+
+FXOSC = 32000000.0
+FSTEP = (FXOSC / 524288)
+frf = 868 #frequ en MHz
+
+def read_one(register):
+	out = spi.xfer([register] + [0])
+	return out[1]
+	
+def write_one(register, payload):
+	print 'on ecrit 0x{:X} dans le register 0x{:X}'.format(payload, register)
+	out = spi.xfer([register | 0x80 , payload])
+	
+def lecture_rx():
+	rx_len = read_one(0x13)#REG_13_RX_NB_BYTES -> taille du packet recu
+	cur_adr = read_one(0x10) #REG_10_FIFO_RX_CURRENT_ADDR	
+	print 'RX!!! len {:d} cur_adr 0x{:X}'.format(rx_len, cur_adr)
+	write_one(0x0d , cur_adr) #REG_0D_FIFO_ADDR_PTR "FIFO SPI pointer"
+	#out = spi.xfer([0x00], rx_len) #REG_00_FIFO = 0x00 "FIFO r/w access"
+	#print(out)
+	
+
+#initialisation du bus spi
 spi = spidev.SpiDev()
 #bus, device -> /dev/spidev<bus>.<device> -> /dev/spidev0.0
 spi.open(0, 0)
 #spi.mode=0b00 #pas indispensable                  
 spi.max_speed_hz = 5000 #indispensable !
 
-resp = spi.xfer([0x01, 0x00])
-print '0x{:X} -- {:#010b}'.format(resp[1], resp[1])
+resp = read_one(0x01)  #REG_01_OP_MODE                                    
+print '0x{:X} -- {:#010b}'.format(resp, resp)
 
-resp2 = spi.xfer([0x06, 0x00])
-print '0x{:X} -- {:#010b}'.format(resp2[1], resp2[1])
+write_one(0x01, 0x80) #REG_01_OP_MODE -> long range mode (LoRa) (p 108 )
+#sleep(0.1) #tester voir si necessaire
 
+resp = read_one(0x01)                                      
+print '0x{:X} -- {:#010b}'.format(resp, resp)
 
+write_one(0x01 , 0x01) #REG_01_OP_MODE -> mode STDBY (p108)
+#sleep(0.1) #tester voir si necessaire
 
+#set frequency
+frf = int((frf * 1000000.0) / FSTEP)
+write_one(0x06 , (frf >> 16) & 0xff)
+write_one(0x07 , (frf >> 8) & 0xff  )
+write_one(0x08 , frf & 0xff)
+sleep(0.1)
+
+#modem config Bw125Cr45Sf128 (0x72, 0x74, 0x04)
+write_one(0x1d , 0x72) #REG_1D_MODEM_CONFIG1
+write_one(0x1e , 0x74) #REG_1E_MODEM_CONFIG2
+write_one(0x26 , 0x04) #REG_26_MODEM_CONFIG3
+
+write_one(0x12 , 0xff) #REG_12_IRQ_FLAGS clear ses flags
+
+write_one(0x01 , 0x05) #mode RxContinuous
+
+while True:
+	print 'IRQ FLAGS: {:#010b}'.format(read_one(0x12)) #REG_12_IRQ_FLAGS
+	if(read_one(0x12) & 0x40): # RX_DONE flag is up!
+		lecture_rx()
+		write_one(0x12 , 0xff) #REG_12_IRQ_FLAGS clear ses flags
+	sleep(1)
 
 spi.close()
