42e45733cdc00151d98944621a7a810b78209ae0
dnagis <vincent.achard@gmail.com>
2020-04-06 12:34:25 +0200

    un peu de travail courant

--- i2c/ina219/i2c_pyftdi_ina219.py
+++ i2c/ina219/i2c_pyftdi_ina219.py
@@ -5,9 +5,21 @@
 #https://github.com/chrisb2/pi_ina219/
 #https://github.com/chrisb2/pi_ina219.git
 #"PGA" = Programmable Gain Amplifier
-#R100 sur la résistance voudrait dire 0.1 ohms. La logique: 100R = 100ohms. R est utilisé comme marqueur de decimal point (moins de risque de se
-#	perdre sur un dessin industriel pendant conversion de taille de fichier). 
 #Le breakout violet de chez adafruit: https://cdn-learn.adafruit.com/downloads/pdf/adafruit-ina219-current-sensor-breakout.pdf
+#la resistance Rshunt sur le breakout vient d'adafruit. Texas Instrument ne fabrique que l'ina219. 
+#"R100" ("R" est the decimal place) sur la résistance =>  0.1 ohms. La logique: 100R = 100ohms. R est utilisé comme marqueur de decimal point (moins de risque de se
+#	perdre sur un dessin industriel pendant conversion de taille de fichier). 
+
+
+
+
+#ToDo
+#Measure du current. Comme référence il me faut la lecture avec le multimètre. Avec la LED qui s'allume! 
+#Et démystifier: est ce que l'intensité mesurée en n'importe quel
+#endroit du circuit est censée être identique? Et est ce que je peux calculer l'intensité attendue? Après s'attaquer aux calculs de la calibration ina219.
+
+
+
 
 from pyftdi.i2c import I2cController, I2cNackError
 from binascii import hexlify
@@ -52,7 +64,7 @@ CONFIG = data[0] << 8 | data[1] #data[0] -> byte du haut (most significant)
 print("config avant = {:08b} {:08b} ({:x})".format(data[0], data[1], CONFIG)) #pour contrôle: au reset doit être à 0x399F
 
 #Ecriture nouvelle config
-#default config: 0011100110011111 (399f)
+# default config: 0011100110011111 (399f)
 new_config = int('0011100110011111',2).to_bytes(2,byteorder='big')
 #slave.write_to(CONFIG_ADDR, b'\x00\x00') -> c'est ce format qu'il faut pour écrire dans le register
 slave.write_to(CONFIG_ADDR, new_config)
@@ -66,60 +78,56 @@ print("config apres = {:08b} {:08b} ({:x})".format(data[0], data[1], CONFIG)) #p
 
 
 
-#Calibration
-#J'ai du courant toujours à zero, normal selon DS: il faut remplir calibration register
-#Logique: la resistance sur le breakout vient d'adafruit. Texas Instrument ne fabrique que l'ina219. Il faut dire à l'ina
-#quelle est la valeur de la résistance.
-#Programming page 5
+
+#J'ai du courant toujours à zero, normal selon DS: il faut remplir calibration register (par défaut à 0)
+
+#Programming page calibration 5
 #La map du register calibration est page 24
 
 #max_possible_amps = shunt_volts_max / self._shunt_ohms ina219.py li 283
-#max_possible_amps = 32 / 100 #j'adapte. -> 32V de range
-#current_lsb = max_possible_amps / 32767
-#calibration = trunc(0.04096 / (current_lsb * 100)) #DS p.12 + ina219.py li 302
-
-#ToDo calibration:
-#trouver la valeur de la résistance du breakout, ma mesure??? -> je trouve 100 milli ohms
-#Trouver comment écrire la calibration dans le register en prenant compte que le premier bit est à 0
-#	 (DS? librairie?)
+max_possible_amps = 32 * 10 #j'adapte. -> 32V de range
+current_lsb = max_possible_amps / 32767
+calibration = trunc(0.04096 / (current_lsb * 0.1)) #DS p.12 + ina219.py li 302
 
 
 
+print("calibration= 0x{:x} decimal {:d}".format(calibration, calibration))
 
-#print("calibration= 0x{:x} decimal {:d}".format(calibration, calibration))
+#int.to_bytes(length, byteorder, *, signed=False) (41).to_bytes(2, byteorder='big') #Créer un bytearray de taille 2 
 
-#int.to_bytes(length, byteorder, *, signed=False) (41).to_bytes(2, byteorder='big') Créer un bytearray de taille 2 
-
-#cal_to_write = calibration.to_bytes(2, byteorder='big') 
-#print("bits calibration qu'on va ecrire: {:08b} {:08b}".format(cal_to_write[0], cal_to_write[1]))
+cal_to_write = calibration.to_bytes(2, byteorder='big') 
+print("bits calibration qu'on va ecrire: {:08b} {:08b}".format(cal_to_write[0], cal_to_write[1]))
 #Ecrire la calibration dans le register
-#Attention le bit0 "is a void bit and always be 0: calibration is the value stored in FS:15:FS1 DS p 24
+#le bit0 "is a void bit and always be 0: calibration is the value stored in FS:15:FS1 DS p 24 -> je ne sais pas comment on fait???
 
-#slave.write_to(CALIBRATION_ADDR, cal_to_write)
+slave.write_to(CALIBRATION_ADDR, cal_to_write)
 #time.sleep(0.5)
-#calib_reg = slave.read_from(CALIBRATION_ADDR,2)
-#CALIB_REG = calib_reg[0] << 8 | calib_reg[1]
-#print("calibration reg lecture = {:#010b} {:#010b} ({:x})".format(calib_reg[0], calib_reg[1], CALIB_REG)) 
+calib_reg = slave.read_from(CALIBRATION_ADDR,2)
+CALIB_REG = calib_reg[0] << 8 | calib_reg[1]
+print("calibration reg lecture = {:#010b} {:#010b} ({:x})".format(calib_reg[0], calib_reg[1], CALIB_REG)) 
 
 
+
+
+
+
+
+#En cours!
 def lire_current():
 	data = slave.read_from(CURRENT_ADDR,2)
 	RAW_DATA = data[0] << 8 | data[1]	
 	#print("{:#010b} {:#010b}".format(data[0], data[1]))
-	sys.stdout.write("current = {:#010b} {:#010b}   \r".format( data[0], data[1]))
-	#RESULT = twos_comp(RAW_DATA) * .01 #LSB = 4 mv pour le bus, 10 µV pour le shunt. Datasheet p.23
-	#print("{:.2f}".format(RESULT))	
-
-
-#ToDo voltage:
-#Changement de gain comprendre
+	#sys.stdout.write("current = {:#010b} {:#010b}   \r".format( data[0], data[1]))
+	RESULT = twos_comp(RAW_DATA) * current_lsb * 1000 #Datasheet p.23
+	sys.stdout.write("{:.2f} \r".format(RESULT))	
 
 
 
-#Bien distinguer le voltage du BUS et celui du SHUNT
+#J'arrive à avoir le même output que si je place les contacteurs du multimètre aux bornes de la résistance de shunt sur le breakout
+#Bien distinguer le voltage du BUS et celui du SHUNT (la resistance visible)
 #Voltage Shunt: 
 #avec le gain par défaut ( PGA= /8 ) c'est le cas de la figure 20 DS. p.21. Seul un bit (le MSB) contient le sign.
-#j'obtiens la correspondance bits<->voltage décrite pp.21 et tableau p. 22 en faisant un twos_complement
+#j'obtiens la correspondance bits <-> voltage décrite pp.21 et tableau p. 22 en faisant un twos_complement
 def lire_voltage_shunt():
 	data = slave.read_from(SHUNT_VOLT_ADDR,2)
 	RAW_BITS = data[0] << 8 | data[1]
@@ -131,7 +139,8 @@ def lire_voltage_shunt():
 	#	ref: DS p 12 (en bas) + p.23 et github.com/chrisb2/pi_ina219.git dans ina219.py li. 359
 
 while(True):
-	lire_voltage_shunt()
+	lire_current()
+	#lire_voltage_shunt()
 	time.sleep(0.1)
 	
 
