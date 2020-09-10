a192a0fa5016095d5606bfc60dab7e6d3355871c
dnagis <vincent.achard@gmail.com>
2020-09-10 10:05:36 +0200

    tasks pour le temps, pour le relevé du hall sensor

--- esp/esp_anemo_hall.c
+++ esp/esp_anemo_hall.c
@@ -4,6 +4,13 @@
  * Lecture built-in hall sensor
  * https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html
  * 
+ * visualisation gnuplot: printf("%i\n", hall); en monitor: ctrl-T + ctrl-L (log to file)
+ * puis gnuplot> plot 'log.anemo.20200910095412.txt'
+ * 
+ * 
+ * 
+ * 
+ * 
  * */
 
 
@@ -13,15 +20,37 @@
 
 #include "driver/adc.h"
 
+int count;
+
+static void task_time_counter(void* arg)
+{
+    while (1) {
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        printf("count = %i\n", count);
+        count = 0;
+    }
+}
+
+
+
+static void task_hall(void* arg)
+{
+    while (1) {
+		int hall = hall_sensor_read();
+		if(hall>85) count++;
+        //printf("%i\n", hall);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
 
 void app_main(void)
 {
-    
+    count = 0;
     //adc1_config_width(ADC_WIDTH_BIT_12);
     
-    while (1) {
-		int hall = hall_sensor_read();
-        printf("hall: %i\n", hall);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(task_hall, "task_hall", 2048, NULL, 10, NULL);
+    xTaskCreate(task_time_counter, "task_time_counter", 2048, NULL, 10, NULL);
+    
+
 }
